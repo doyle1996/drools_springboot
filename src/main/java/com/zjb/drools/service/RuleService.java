eba3c484c0e9890fcc2f0220c2e819f0896e5df1
doyle1996 <1184125133@qq.com>
2021-06-24 15:47:31 +0800

    Spring Boot整合Drools

---
+++ src/main/java/com/zjb/drools/service/RuleService.java
@@ -0,0 +1,21 @@
+package com.zjb.drools.service;
+
+import org.kie.api.KieBase;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class RuleService {
+    @Autowired
+    private KieBase kieBase;
+    public void rule(){
+        KieSession kieSession = kieBase.newKieSession();
+        kieSession.fireAllRules();
+        kieSession.dispose();
+
+
+    }
+}
